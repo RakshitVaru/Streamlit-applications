49275576c08117658077dd40966d86ad698f45ba
RakshitVaru <rakshitvaru97@gmail.com>
2021-09-20 14:25:24 -0300

    Multi-page Application

---
+++ multi-page-application/apps/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.title("Home")
+    st.write("This is the `Home Page` of this multi-page app.")
+    st.write("In this app, we will be building a simple classification model using the Iris dataset.")
